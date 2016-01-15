77d17adf4aeb1a7c89a51993bd7897e2599b6ac1
pfanchri <christofpfannenmuller@MacBook-3.local>
2016-01-15 15:02:37 +0100

    AVR Studio Projekt eröffnet

---
+++ Software/DIYWordclock/CProgram1.c
@@ -0,0 +1,6 @@
+/*
+ * CProgram1.c
+ *
+ * Created: 15.01.2016 14:57:25
+ *  Author: christofpfannenmulle
+ */ 
---
+++ Software/DIYWordclock/main.c
@@ -0,0 +1,6 @@
+/*
+ * main.c
+ *
+ * Created: 15.01.2016 14:58:54
+ *  Author: christofpfannenmulle
+ */ 
